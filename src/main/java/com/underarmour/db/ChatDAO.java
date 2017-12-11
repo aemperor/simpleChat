c8b6eb872b950f918a8a73176fa83b0bc3c3d1bf
Alexis Emperador <alexis.emperador@sailpoint.com>
2017-12-10 22:16:50 -0600

    able to add new chats to the heroku database, cleaned up config.yml

--- src/main/java/com/underarmour/db/ChatDAO.java
+++ src/main/java/com/underarmour/db/ChatDAO.java
@@ -1,11 +1,9 @@
 package com.underarmour.db;
 
-import com.underarmour.model.Chat;
+import org.postgresql.util.PSQLException;
+import org.skife.jdbi.v2.exceptions.UnableToExecuteStatementException;
 import org.skife.jdbi.v2.sqlobject.Bind;
-import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
-
-import java.util.Optional;
 
 /**
  * Chat Data Access Object
@@ -13,8 +11,8 @@ import java.util.Optional;
 public interface ChatDAO {
 
 	@SqlUpdate("CREATE TABLE chat (id INTEGER PRIMARY KEY, username VARCHAR, text VARCHAR, timeout INTEGER)")
-	public void createTable();
+	public void createTable() throws UnableToExecuteStatementException;
 
 	@SqlUpdate("INSERT INTO chat(id, username, text, timeout) VALUES (:id, :username, :text, :timeout)")
-	void insertNamed(@Bind("id") int id, @Bind("username") String username, @Bind("text") String text, @Bind("timeout") long timeout);
+	void insertNamed(@Bind("id") long id, @Bind("username") String username, @Bind("text") String text, @Bind("timeout") long timeout);
 }
--- src/main/java/com/underarmour/resources/ChatResource.java
+++ src/main/java/com/underarmour/resources/ChatResource.java
@@ -2,29 +2,57 @@ package com.underarmour.resources;
 
 import com.underarmour.db.ChatDAO;
 import com.underarmour.model.Chat;
+import org.skife.jdbi.v2.exceptions.UnableToExecuteStatementException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
 * Resource to house chat apis
 */
 @Path("/chat")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ChatResource {
+	private final Logger LOGGER = LoggerFactory.getLogger(ChatResource.class);
 	private final ChatDAO chatDAO;
 
 	public ChatResource(ChatDAO chatDAO) {
 		this.chatDAO = chatDAO;
+		try {
+			chatDAO.createTable();
+		}
+		catch(UnableToExecuteStatementException ex) {
+			LOGGER.info("Table with name chat already exists");
+		}
 	}
 
+	/**
+	 * Creates a new chat object.
+	 * @param chat The chat to be created.
+	 * @return The id of the newly created chat.
+	 */
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String create(Chat chat) {
-		return "created";
+	public Response create(Chat chat) {
+		long id = generateId();
+
+		chatDAO.insertNamed(id, chat.getUser(), chat.getText(), chat.getTimeout());
+
+		Map entity = new HashMap();
+		entity.put("id", id);
+
+		return Response.status(Response.Status.CREATED).entity(entity).build();
 	}
 
 	@GET
@@ -33,9 +61,26 @@ public class ChatResource {
 		return "001";
 	}
 
-	@GET
-	@Path("{username}")
-	public String get(@PathParam("username") String username) {
-		return "001";
+//	@GET
+//	@Path("{username}")
+//	public String get(@PathParam("username") String username) {
+//		return "001";
+//	}
+
+	/**
+	 * Generates random id within the bounds of 0 to 1,000,000.
+	 * @return The generated id.
+	 */
+	private long generateId() {
+		int UPPER_BOUND = 1000000;
+		Random random = new Random();
+		long nextId = random.nextInt(UPPER_BOUND);
+
+		// positive ids only
+		while (nextId < 0) {
+			nextId = random.nextInt(1000000);
+		}
+
+		return nextId;
 	}
 }
